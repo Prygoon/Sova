daac28bef40e65a5c6fba9a955c2c8b5602ac4bb
Prygoon <prbigoon@gmail.com>
2017-01-15 03:07:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/prygoon/newsova/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.prygoon.newsova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    ImageView owlImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setText(R.string.show_owl);
+        owlImage = (ImageView) findViewById(R.id.imageView);
+        owlImage.setVisibility(View.INVISIBLE);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (owlImage.getVisibility() == View.INVISIBLE) {
+                    owlImage.setVisibility(View.VISIBLE);
+                    button1.setText(R.string.hide_owl);
+                } else {
+                    owlImage.setVisibility(View.INVISIBLE);
+                    button1.setText(R.string.show_owl);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
